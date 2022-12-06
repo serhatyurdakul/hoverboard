7e4be47eb76128dd5e91ffe56d95b3e24dbd941b
Serhat <serhat@Serhat-MacBook-Air.local>
2022-12-07 01:12:30 +0300

    Hoverboard
    Simple HTML, CSS and JavaScript Project

---
+++ app.js
@@ -0,0 +1,37 @@
+const container = document.getElementById("container");
+const colors = [
+  "#ff0000",
+  "#0004ff",
+  "#09ff00",
+  "#fdff00",
+  "#ff00e7",
+  "#ffb400",
+  "#e1225b",
+];
+
+const SQUARES = 460;
+
+for (let i = 0; i < SQUARES; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => setColor(square));
+  square.addEventListener("mouseout", () => removeColor(square));
+
+  container.appendChild(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.background = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.background = "#303443";
+  element.style.boxShadow = "0 0 2px #000";
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
